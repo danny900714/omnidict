02936d5b2457f371121f91a096099238b248c510
Chao Tzu-Hsien <danny900714@gmail.com>
2026-05-30 23:55:32 +0800

    Ask user whether to override the field content

--- src/cambridge_dictionary/__init__.py
+++ src/cambridge_dictionary/__init__.py
@@ -1,14 +1,15 @@
 # Include vendor directory into sys.path
 import sys
 from pathlib import Path
+from typing import Any
 
 vendor_path = str(Path(__file__).parent / "vendor")
 if vendor_path not in sys.path:
     sys.path.insert(0, vendor_path)
 
 from aqt import gui_hooks, mw
-from aqt.editor import Editor
-from aqt.utils import show_info, show_warning
+from aqt.editor import Editor, Note
+from aqt.utils import show_info, show_warning, ask_user
 
 from .cambridge import Client
 
@@ -17,6 +18,12 @@ client = Client()
 
 
 def fetch_definition(editor: Editor) -> None:
+    def fetch_and_set_definition(vocabulary: str, fields: list[Any], current_field: int) -> None:
+        config = mw.addonManager.getConfig(__name__)
+        definition = client.fetch_definition(config["dict_code"], vocabulary)
+        fields[current_field] = definition.render_html()
+        editor.loadNoteKeepingFocus()
+
     def after_save():
         current_field = editor.currentField
         if current_field is None or editor.note is None:
@@ -31,15 +38,12 @@ def fetch_definition(editor: Editor) -> None:
             show_warning("Please enter a word in the field above to proceed.")
             return
         if editor.note.fields[current_field]:
-            show_warning("Current field is not empty.")
+            ask_user("Will overwrite existing content in the current field. Do you want to proceed?",
+                     lambda ok: fetch_and_set_definition(vocabulary, editor.note.fields, current_field) if ok else None)
             return
 
-        config = mw.addonManager.getConfig(__name__)
-        definition = client.fetch_definition(config["dict_code"], vocabulary)
-        editor.note.fields[current_field] = definition.render_html()
-        editor.loadNoteKeepingFocus()
+        fetch_and_set_definition(vocabulary, editor.note.fields, current_field)
 
-    print("fetch_definition")
     editor.call_after_note_saved(after_save)
 
 
